495e126e092eaf6fe138b7bc6b2e9ad70581b5c8
AnushMelkonyan <melkonyananushbk@gmail.com>
2020-09-02 11:55:31 +0400

    drag and drop functionality

--- deleteDrugDropCars/carsTableList.js
+++ deleteDrugDropCars/carsTableList.js
@@ -1,48 +1,47 @@
 const carModel = [
-    "Model",
-    "Brand",
-    "Date",
-    "Horsepower",
-    "Transmission",
-    "Class",
+    `Model`,
+    `Brand`,
+    `Date`,
+    `Horsepower`,
+    `Transmission`,
+    `Class`,
 ];
-carModel.push("Delete")
-let body = document.getElementsByTagName('body')[0];
-let div = document.createElement('div')
-let divPagination = document.createElement('div');
+carModel.push(`ADD`)
+let body = document.getElementsByTagName(`body`)[0];
+let div = document.createElement(`div`)
+let divPagination = document.createElement(`div`);
 body.appendChild(div);
 body.appendChild(divPagination);
-let table = document.createElement('table');
-table.setAttribute("id", "table")
+let table = document.createElement(`table`);
+table.setAttribute(`id`, `table`)
 const notesOnPage = 10;
-let car = JSON.parse(localStorage.getItem("carList"))
+let car = JSON.parse(localStorage.getItem(`carList`))
 let ul;
 
 createTable(car.slice(0, notesOnPage));
 deleteRows();
 
 function createTable(cars) {
-    let title = document.createElement('tr')
+    let title = document.createElement(`tr`)
 
-    for(let i = 0; i < carModel.length; i++){
-        let th = document.createElement('th')
-        th.setAttribute("draggable", "true")
-        th.setAttribute('ondragover', 'onDragOver(event)')
-        th.setAttribute('ondrop', 'onDrop(event)')
-        th.setAttribute('ondragstart', 'onDragStart(' + i + ', event)')
-        th.setAttribute("id", `elem${i}`)
+    for (let i = 0; i < carModel.length; i++) {
+        let th = document.createElement(`th`)
+        th.setAttribute(`draggable`, `true`)
+        th.setAttribute(`ondragover`, `onDragOver()`)
+        th.setAttribute(`ondrop`, `onDrop(event)`)
+        th.setAttribute(`ondragstart`, `onDragStart(${i}, event)`)
         th.innerText = (carModel[i]);
         title.appendChild(th);
     }
 
     table.appendChild(title);
 
-    for(let i = 0; i < cars.length; i++) {
-        let tr = document.createElement('tr');
-        for(let j = 0; j < carModel.length; j++){
-            let td = document.createElement('td');
-            td.setAttribute("name", `td${j}`);
-            cars[i].Delete = "Delete";
+    for (let i = 0; i < cars.length; i++) {
+        let tr = document.createElement(`tr`);
+        for (let j = 0; j < carModel.length; j++) {
+            let td = document.createElement(`td`);
+            td.setAttribute(`name`, `td${j}`);
+            cars[i].ADD = `Delete`;
 
             td.innerText = cars[i][carModel[j]];
 
@@ -52,50 +51,50 @@ function createTable(cars) {
     }
     div.appendChild(table);
 
-    divPagination.innerHTML = ""
+    divPagination.innerHTML = ``
     createPagination();
     deleteRows()
 }
 
-function createPagination () {
+function createPagination() {
     let amountPages = Math.ceil(car.length / notesOnPage)
 
-    let ul = document.createElement('ul')
-    ul.setAttribute("id", "pagination")
+    let ul = document.createElement(`ul`)
+    ul.setAttribute(`id`, `pagination`)
 
     let items = []
-    for(let i = 1; i <= amountPages; i++){
-        let li = document.createElement('li')
+    for (let i = 1; i <= amountPages; i++) {
+        let li = document.createElement(`li`)
         li.innerHTML = i;
         ul.appendChild(li)
         items.push(li)
     }
 
-    for(let item of items){
-         item.addEventListener('click', function(){
-            let active = document.querySelector('#pagination li.active')
-            if(active){
-                active.classList.remove('active')
+    for (let item of items) {
+        item.addEventListener(`click`, function () {
+            let active = document.querySelector(`#pagination li.active`)
+            if (active) {
+                active.classList.remove(`active`)
             }
-            this.classList.add('active')
+            this.classList.add(`active`)
 
             let pageNum = +this.innerHTML
 
             let start = (pageNum - 1) * notesOnPage;
             let end = start + notesOnPage;
             let notes = car.slice(start, end)
-            table.innerHTML = ""
+            table.innerHTML = ``
             createTable(notes);
-         })   
+        })
     }
     divPagination.appendChild(ul)
 }
 
-function deleteRows(){
-    for(let i = 1; i < table.rows.length; i++){
-        table.rows[i].cells[carModel.length - 1].onclick = function (){
-            let question = confirm("Do you want to delete this row?");
-            if(question === true){
+function deleteRows() {
+    for (let i = 1; i < table.rows.length; i++) {
+        table.rows[i].cells[carModel.length - 1].onclick = function () {
+            let question = confirm(`Do you want to delete this row?`);
+            if (question === true) {
                 let index = this.parentNode.rowIndex;
                 table.deleteRow(index);
                 car.splice(index, 1);
@@ -104,39 +103,29 @@ function deleteRows(){
     }
 }
 
-// const carModel = [
-//     "Model",
-//     "Brand",
-//     "Date",
-//     "Horsepower",
-//     "Transmission",
-//     "Class",
-// ];
-
-let currentIndex;
 function onDragStart(index, event) {
-    event.dataTransfer.setData('index', index);   // event.target.id dtanuma en elementi id vory vory eventy arela
+    event.dataTransfer.setData(`index`, index);
 }
 
-
-function onDragOver(index){
+function onDragOver() {
     event.preventDefault();
-  //  console.log(index.dataTransfer.setData("text"))
 }
 
+function onDrop() {
+    let data = event.dataTransfer.getData(`index`)
 
-
-function onDrop(){
-    let data = event.dataTransfer.getData("index")
-    
     console.log(data)
-    console.log(event)
+    console.log(carModel.indexOf(event.target.innerHTML))
 
+    carModel.indexOf(event.target.innerHTML[fromIndex = 0])
+    changeArray(carModel, data, carModel.indexOf(event.target.innerHTML))
 }
 
-//event.target.className.split("/\s")
-
-    // let dragArr = [];
-    // for(i = 0; i < cars.length; i++){
-    //     dragArr.push(cars[i].Model)
-    // }
+function changeArray(arr, arg1, arg2) {
+    
+    arr.splice(arg2, 0);
+   // arr[arg1] = arr.splice(arg2, 1, arr[arg1])[0];
+    console.log(arr)
+    table.innerHTML = ``
+    createTable(car.slice(0, notesOnPage))
+}
